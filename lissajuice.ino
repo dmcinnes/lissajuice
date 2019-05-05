b319e8fa88cb8e4c4566709578b3faec21642b7f
Doug McInnes <doug@dougmcinnes.com>
2019-05-04 20:54:26 -0700

    switch back to 64 MHz PLL on Timer1

--- lissajuice.ino
+++ lissajuice.ino
@@ -24,20 +24,21 @@ volatile unsigned int Acc1, Acc2, Jump;
 volatile signed int X, Y;
 
 void SetupDDS () {
+  // Enable 64 MHz PLL and use as source for Timer1
+  PLLCSR = 1<<PCKE | 1<<PLLE;
+
+  // Set up Timer/Counter1 for PWM output
   TIMSK = 0;                               // Timer interrupts OFF
+  TCCR1 = 1<<PWM1A | 2<<COM1A0 | 1<<CS10;  // PWM A, clear on match, 1:1 prescale
+  GTCCR = 1<<PWM1B | 3<<COM1B0;            // PWM B, clear on match
+  pinMode(1, OUTPUT);                      // Enable OC1A PWM output pin
+  pinMode(4, OUTPUT);                      // Enable OC1B PWM output pin
 
-  // Set up Timer/Counter0 for PWM output
-  TCCR0A = 3<<COM0A0 | 3<<COM0B0 | 3<<WGM00; // Clear on match A&B, Fast PWM, 1:1 prescale
-  TCCR0B = 0<<WGM02 | 1<<CS00;
-
-  pinMode(0, OUTPUT);                      // Enable OC0A PWM output pin
-  pinMode(1, OUTPUT);                      // Enable OC0B PWM output pin
-
-  // Set up Timer/Counter1 for 20kHz interrupt to output samples.
-  TCCR1 = 1<<PWM1A | 4<<CS10;  // PWM A, 1/8 prescale
-
-  TIMSK = 1<<OCIE1A;                       // Enable compare match, disable overflow
-  OCR1A = 60;                              // Divide by 61
+  // Set up Timer/Counter0 for 20kHz interrupt to output samples.
+  TCCR0A = 3<<WGM00;                       // Fast PWM
+  TCCR0B = 1<<WGM02 | 2<<CS00;             // 1/8 prescale
+  TIMSK = 1<<OCIE0A;                       // Enable compare match, disable overflow
+  OCR0A = 60;                              // Divide by 61
 }
 
 // Calculate sine wave
@@ -53,11 +54,11 @@ void CalculateSine () {
 void Sine () {
   Acc1 = Acc1 + Jump;
   Acc2 = Acc2 + 3 * Jump + 2;
-  OCR0A = Sinewave[Acc1>>8] + 128;
-  OCR0B = Sinewave[Acc2>>8] + 128;
+  OCR1A = Sinewave[Acc1>>8] + 128;
+  OCR1B = Sinewave[Acc2>>8] + 128;
 }
 
-ISR(TIM1_COMPA_vect) {
+ISR(TIM0_COMPA_vect) {
   Sine();
 }
 
